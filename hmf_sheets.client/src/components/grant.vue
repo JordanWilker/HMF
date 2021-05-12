960e8e085ab9683b4276a99ee5ece4b4a5454244
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-05-12 15:38:39 -0600

    starting front end

---
+++ hmf_sheets.client/src/components/grant.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Grants">
+    HOWDY HOW GOES IT PARDNER
+    {{ grants.name }}
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from 'vue'
+import { AppState } from '../AppState'
+export default {
+  name: 'Grants',
+  props: {
+    grants: {
+      type: Object,
+      default: () => new Grant()
+    }
+  },
+  setup() {
+    const state = reactive({
+      grants: computed(() => AppState.grants)
+    })
+    return { state }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
